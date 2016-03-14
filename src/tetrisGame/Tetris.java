145c169fdbaee3677ca5ea9fa1551cd7881aed86
dijatazz <dabo@et.esiea.fr>
2016-03-14 10:40:46 +0100

    Création des premières classes

---
+++ src/tetrisGame/Tetris.java
@@ -0,0 +1,44 @@
+package tetrisGame;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+//Informations sur les pièces de Tetris
+
+public class Tetris extends JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JLabel statusBar;
+
+
+    public Tetris() {
+
+        statusBar = new JLabel("Score 0");
+        add(statusBar, BorderLayout.NORTH);
+        Board board = new Board(this);
+        add(board);
+        board.start();
+
+        setSize(200, 400);
+        setTitle("Tetris Game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+   }
+    
+    public JLabel getstatusBar() {
+        return statusBar;
+    }
+
+     public static void main(String[] args) {
+
+         Tetris game = new Tetris();
+         game.setLocationRelativeTo(null);
+         game.setVisible(true);
+
+     } 
+ }
--- src/tetrisGame/test.java
+++ src/tetrisGame/test.java
@@ -1,3 +1,4 @@
+package tetrisGame;
 
 public class test {
 
